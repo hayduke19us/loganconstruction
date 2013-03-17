5488ab234397383526f3058247c521c6f076cc2d
hayduke19us <hayduke19us@gmail.com>
2013-03-17 03:33:30 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Logan::Application.config.secret_token = '3a1a376ac0726544e0c37683b11353fe7b5d7cc0e8c8f7cd18ddd1239d7355d59d193a24d71325080f897ce304b62d35caee77b1161e9791700232b4ed62d3b6'
