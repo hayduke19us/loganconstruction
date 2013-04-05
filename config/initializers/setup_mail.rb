4949c46601a4782711e36f8e84570003940a1aff
hayduke19us <hayduke19us@gmail.com>
2013-04-05 18:56:31 -0500

    fixed mailer with sendgrid:

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,10 +1,11 @@
 ActionMailer::Base.smtp_settings = {
   :address              => "smtp.sendgrid.net",
   :port                 => 587,
-  :domain               => "sendgrid.com",
+  :domain               => "loganconstruction.herokuapps.com",
   :user_name            => "hayduke19us",
   :password             => "rainflood",
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
 
+ActionMailer::Base.default_url_options[:host] = "loganconstruction.herokuapps.com"
